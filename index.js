37ca6dd0c00792a636c3926a9b7a88c884857e3b
ramus <ramussiz29@gmail.com>
2023-07-28 16:00:13 +0100

    change ruturn syntax, the fonction return 3 differentes messages separated by |

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function mixedMessages(){
 
         let messagesRandom =Math.floor(Math.random() * 6); //For generating an index of the arrayMessage
-        let firstArrayMessages=[ //list of messages
+        let firstArrayMessages=[ //first arry of messages
                             'La preuve de la valeur d\'un système informatique est son existence',
                             'Je m\'en fous si ça marche sur votre machine ! Nous ne livrons pas votre machine',
                             'Marcher sur l\'eau et développer un logiciel à partir d\'une spécification sont faciles si les deux sont gelés',
@@ -9,7 +9,7 @@ function mixedMessages(){
                             'Entre le bit, unité de mesure informatique, et les queues de sondage, le circuit imprimé est un peu sexiste',
                             'Quelle prétention de prétendre que l\'informatique est récente : Adam et Eve avaient déjà un Apple !'
                         ];
-        let secondArrayMessages=[ //list of messages
+        let secondArrayMessages=[ //second array of messages
                         'Si vous ne pouvez expliquer un concept à un enfant de six ans, c\'est que vous ne le comprenez pas complètement',
                         'Regardez profondément dans la nature, et alors vous comprendrez tout beaucoup mieux',
                         'L\'intelligence n\'est pas la capacité de stocker des informations, mais de savoir où les trouver',
@@ -18,20 +18,20 @@ function mixedMessages(){
                         'Le monde est dangereux à vivre ! Non pas tant à cause de ceux qui font le mal, mais à cause de ceux qui regardent et laissent faire'
                     ];
 
-        let thirdArrayMessages=[ //list of messages
+        let thirdArrayMessages=[ //third array of messages
                     'Il n\'existe que deux choses infinies, l\'univers et la bêtise humaine… mais pour l\'univers, je n\'ai pas de certitude absolue',
                     'Placez votre main sur un poêle une minute et ça vous semble durer une heure. Asseyez vous auprès d\'une jolie fille une heure et ça vous semble',
                     'La valeur d\'un homme tient dans sa capacité à donner et non dans sa capacité à recevoir',
                     'N\'essayez pas de devenir un homme qui a du succès. Essayez de devenir un homme qui a de la valeur',
                     'Le mot progrès n\'aura aucun sens tant qu\'il y aura des enfants malheureux',
-                    'Un problème créé ne peut être résolu en réfléchissant de la même manière qu’il a été créé'
+                    'Un problème créé ne peut être résolu en réfléchissant de la même manière qu\'il a été créé'
                 ];
 
         let firstMessage = firstArrayMessages[messagesRandom];
         let secondMessage = secondArrayMessages[messagesRandom];
         let thirdMessage = thirdArrayMessages[messagesRandom];
     
-        return {firstMessage, secondMessage, thirdMessage} ;      
+        return `${firstMessage}| ${secondMessage}| ${thirdMessage} ` ;  //return  3 differents messages separeted by |  
     }
 
 // calling mixedMessages fonction
