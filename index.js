8804e42e94fbe47ef1c7dbced4cfe10a66c2d4cb
ramus <ramussiz29@gmail.com>
2023-07-28 15:07:36 +0100

    add 2 messages into the arrayMessages

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function mixedMessages(){
 
-        let messagesRandom =Math.floor(Math.random() * 4); //For generating an index of the arrayMessage
+        let messagesRandom =Math.floor(Math.random() * 6); //For generating an index of the arrayMessage
         let arrayMessages=[ //list of messages
                             'La preuve de la valeur d\'un système informatique est son existence',
                             'Je m\'en fous si ça marche sur votre machine ! Nous ne livrons pas votre machine',
                             'Marcher sur l\'eau et développer un logiciel à partir d\'une spécification sont faciles si les deux sont gelés',
-                            'Ajouter des personnes à un projet en retard accroît son retard'
+                            'Ajouter des personnes à un projet en retard accroît son retard',
+                            'Entre le bit, unité de mesure informatique, et les queues de sondage, le circuit imprimé est un peu sexiste.',
+                            'Quelle prétention de prétendre que l\'informatique est récente : Adam et Eve avaient déjà un Apple !'
                         ];
 
         let message = arrayMessages[messagesRandom];
